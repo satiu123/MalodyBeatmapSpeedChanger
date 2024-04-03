1369769a91cda2fe9c1a4c6f011b0dd95827a7f1
satiu <62333904+satiu123@users.noreply.github.com>
2024-04-03 17:06:50 +0800

    Delete Gui/tempCodeRunnerFile.py

--- Gui/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-def start_to_queue(self):
-        self.output_text.clear()
-        addQueue(self.rate_dict)
-        self.rate_dict.clear()
-        thread1=threading.Thread(target=startQueue)
-        thread1.start()
